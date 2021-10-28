91bcefa500b8e67470e6e40b1e4050c1ac0f8205
Alessia9802 <alessiamisciagna@yahoo.it>
2021-10-28 14:49:53 +0200

    Bonus add image

---
+++ Bonus/assets/js/app.js
@@ -0,0 +1,10 @@
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+//BONUS:Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+var root = new Vue({
+  el: "#root",
+  data: {
+    message: "Hello world!",
+    immagine: "https://picsum.photos/id/237/400/300",
+  },
+});
